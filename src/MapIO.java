f68190b72dde6e4c6a5a4c56d49aff15862c2411
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-04-25 18:44:17 +1000

    Add serializeMap method

--- src/MapIO.java
+++ src/MapIO.java
@@ -2,4 +2,22 @@ import java.util.ArrayList;
 import java.io.*;
 
 public class MapIO {
+    public static boolean serializeMap(Room root, String filename) {
+        ObjectOutputStream room_out = null;
+        try {
+            FileOutputStream file_out = new FileOutputStream(filename);
+            room_out = new ObjectOutputStream(file_out);
+            room_out.writeObject(root);
+        } catch (FileNotFoundException e) {
+            return false;
+        } catch (IOException e) {
+            return false;
+        } finally {
+            try {
+                room_out.close();
+            } catch (IOException e) {
+                return false;
+            }
+        }
+        return true;
 }
