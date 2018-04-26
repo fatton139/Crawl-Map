ce7f14c4f054649a75ec62af299fd9e3261f833d
Toe, Knee <leletonyli.13944@yahoo.com.au>
2018-04-26 16:57:26 +1000

    Added saveMap method

--- src/MapIO.java
+++ src/MapIO.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.io.*;
+import java.util.LinkedList;
+import java.util.Set;
 
 public class MapIO {
     public static Room deserializeMap(String filename) {
@@ -22,6 +24,55 @@ public class MapIO {
             }
         }
     }
+    public static boolean saveMap(Room root, String filename) {
+        FileWriter file;
+        BufferedWriter BW = null;
+        BoundsMapper BM = new BoundsMapper(root);
+        BM.walk();
+        ArrayList<Room> rooms = new ArrayList<>(BM.coords.keySet());
+        try {
+            file = new FileWriter(filename);
+            BW = new BufferedWriter(file);
+            //1. The number of rooms in the file
+            BW.write(Integer.toString(rooms.size()));
+            BW.newLine();
+            //2. Room descriptions
+            for (Room room:rooms) {
+                BW.write(room.getDescription());
+                BW.newLine();
+            }
+            //3. Room exits
+            for (Room room:rooms) {
+                BW.write(Integer.toString(room.getExits().keySet().size()));
+                BW.newLine();
+                for (String exit: room.getExits().keySet()) {
+                    BW.write(Integer.toString(rooms.indexOf(room.getExits().get
+                            (exit))));
+                    BW.write(" ");
+                    BW.write(exit);
+                    BW.newLine();
+                }
+            }
+            //4. Room contents
+            for (Room room:rooms) {
+                BW.write(Integer.toString(room.getContents().size()));
+                BW.newLine();
+                for(Thing thing: room.getContents()) {
+                    BW.write(thing.repr());
+                    BW.newLine();
+                }
+            }
+        } catch (IOException e) {
+            return false;
+        } finally {
+            try {
+                if (BW != null)
+                    BW.close();
+            } catch (IOException e) { return false; }
+        }
+        return true;
+    }
+
     public static boolean serializeMap(Room root, String filename) {
         ObjectOutputStream room_out = null;
         try {
